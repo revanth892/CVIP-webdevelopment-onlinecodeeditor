0f6a3a01cf8fee2006bf0612fbb14b25578f4130
revanth892 <revanth9347569169@gmail.com>
2023-12-29 22:48:58 +0530

    made few changes to avoid errors

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-function run()
-{
-    let htmlCode =document.getElementById("html_code")
-    let csscode =document.getElementById("css_code")
-    let jscode=document.getElementById("js_code")
-    let output=document.getElementById("output")
-    output.contentDocument.body.innerHTML=htmlCode.value +"<style>"+ csscode.value +"</style>";
-    output.contentWindow.eval(jscode.value)
+// function run()
+// {
+//     let htmlCode =document.getElementById("html_code")
+//     let csscode =document.getElementById("css_code")
+//     let jscode=document.getElementById("js_code")
+//     let output=document.getElementById("output")
+//     output.contentDocument.body.innerHTML=htmlCode.value +"<style>"+ csscode.value +"</style>";
+//     output.contentWindow.eval(jscode.value)
+// }
 
+
+function run() {
+    let htmlCode = document.getElementById("html_code");
+    let cssCode = document.getElementById("css_code");
+    let jsCode = document.getElementById("js_code");
+    let output = document.getElementById("output");
+
+    if (output.contentDocument) {
+        let combinedCode = `
+            ${htmlCode.value}
+            <style>${cssCode.value}</style>
+            <script>${jsCode.value}</script>
+        `;
+
+        output.contentDocument.open();
+        output.contentDocument.write(combinedCode);
+        output.contentDocument.close();
+    } else {
+        console.error("Failed to access the iframe content.");
+    }
 }
