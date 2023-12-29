370fba9be904e9914326978f9ed5250f8d3ac0da
revanth892 <revanth9347569169@gmail.com>
2023-12-29 18:58:27 +0530

    add html and css code

---
+++ script.js
@@ -0,0 +1,7 @@
+function run()
+{
+    let htmlCode =document.getElementById("html_code")
+    let csscode =document.getElementById("css_code")
+    let jscode=document.getElementById("js_code")
+    let output=document.getElementById("output")
+}
